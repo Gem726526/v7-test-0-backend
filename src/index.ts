a7ae6bbf7edb8b9a64c7c34d94317efcd2a94742
Ranjan <ranjanghimire862@gmail.com>
2022-06-20 17:31:02 +0545

    backend cors error fixed

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
+import cors from 'cors'
+
+import { AuthResolver } from "./resolvers/AuthResolver";
+
+dotenv.config();
+
+const app = express();
+app.use(cors())
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+async function bootstrap() {
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [AuthResolver],
+      validate: true,
+    }),
+    playground: true,
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
+  server.applyMiddleware({ app, cors: false });
+
+  app.listen({ port: PORT }, () => {
+    console.log(`Server is running, GraphQL Playground available at ${PORT}`);
+  });
+}
+
+bootstrap();
